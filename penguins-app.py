1e69e1b3ddf90bfa6949c2b1ad1a425e128b737f
cal284 <76658902+cal284@users.noreply.github.com>
2020-12-29 22:41:00 -0400

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -14,8 +14,6 @@ This app predicts the **Palmer Penguin** species!
 
 
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
-
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
 st.sidebar.header('User Input Features')
